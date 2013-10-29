618d9e353fb972d57e55ad85993200f9b5c67cf6
Nick <nick.young@auckland.ac.nz>
2013-10-29 15:27:15 +1300

    mark expired advisers and researchers on their project pages

--- src/main/java/pm/controller/ViewProjectController.java
+++ src/main/java/pm/controller/ViewProjectController.java
@@ -13,7 +13,9 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
 import pm.db.ProjectDao;
+import pm.pojo.APLink;
 import pm.pojo.ProjectWrapper;
+import pm.pojo.RPLink;
 
 public class ViewProjectController extends AbstractController {
 	
@@ -26,11 +28,25 @@ public class ViewProjectController extends AbstractController {
     	ModelAndView mav = new ModelAndView("viewproject");
 		ProjectWrapper pw = projectDao.getProjectWrapperById(projectId);
 		mav.addObject("pw", pw);
+		Date now = new Date();
+    	DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 		if (!pw.getProject().getEndDate().trim().equals("")) {
-			Date now = new Date();
-	    	DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 			boolean expired = now.after(df.parse(pw.getProject().getEndDate()));
 			mav.addObject("expired",expired);
+		}
+		for (RPLink r:pw.getRpLinks()) {
+			if (!r.getResearcher().getEndDate().trim().equals("")) {
+				if (now.after(df.parse(r.getResearcher().getEndDate()))) {
+					r.getResearcher().setFullName(r.getResearcher().getFullName() + " (expired)");
+				}
+			}
+		}
+		for (APLink a:pw.getApLinks()) {
+			if (a.getAdviser().getEndDate()!=null && !a.getAdviser().getEndDate().trim().equals("")) {
+				if (now.after(df.parse(a.getAdviser().getEndDate()))) {
+					a.getAdviser().setFullName(a.getAdviser().getFullName() + " (expired)");
+				}
+			}
 		}
 		return mav;
 	}
